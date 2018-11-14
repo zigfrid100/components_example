8d618bdb2f84d02e6e8b3643b825ee4a4ba94d43
Kyrylo Sergeiev <zigfrid100@me.com>
2018-11-14 18:55:51 +0100

    html to pdf

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Friend, FRIENDLIST} from '../friends';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-repos',
@@ -12,6 +14,8 @@ export class ReposComponent implements OnInit {
   timeForeach = 0;
   dif = 0.0;
   iterationen = [];
+
+  pagesValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   selectedFriend: Friend;
   friends: Friend[] = FRIENDLIST;
@@ -29,7 +33,6 @@ export class ReposComponent implements OnInit {
 
       const tempForLoopStart = Date.now();
       for (let index = 0; index < this.iterationen.length; index++) {
-        console.log(index);
         if ( index === 50000) {
           break;
         }
@@ -37,10 +40,8 @@ export class ReposComponent implements OnInit {
       const tempForLoopEnd = Date.now();
       this.timeForLoop = (tempForLoopEnd - tempForLoopStart) / 1000;
 
-
       const tempForEachStart = Date.now();
       this.iterationen.forEach(element => {
-        console.log(element);
       });
       const tempForEachEnd = Date.now();
       this.timeForeach = (tempForEachEnd - tempForEachStart) / 1000;
@@ -54,21 +55,41 @@ export class ReposComponent implements OnInit {
       }
 
     }, 1000);
-
-
-
-
-
   }
 
-  isTimeForeachBetter() {
-    let result = 'red';
-
-    if (this.timeForLoop < this.timeForeach) {
-      result = 'green';
-    }
-    return result;
+  getBase64Image(img) {
+    console.log('getBase64Image');
+    const canvas = document.createElement('canvas');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    const dataURL = canvas.toDataURL('../../assets ');
+    return dataURL;
   }
+
+
+captureScreen() {
+  console.log('captureScreen');
+  const doc = new jspdf();
+  for (let i = 0 ; i < this.pagesValue.length - 1 ; i++) {
+   const imageData = this.getBase64Image(document.getElementById('friend' + i));
+   // console.log(imageData);
+     doc.addImage(imageData, 'JPG', 10, (i + 1) * 10, 180, 150);
+     doc.addPage();
+  }
+  doc.save('Test.pdf');
+}
+
+
+isTimeForeachBetter() {
+  let result = 'red';
+
+  if (this.timeForLoop < this.timeForeach) {
+    result = 'green';
+  }
+  return result;
+}
 
 
 }
